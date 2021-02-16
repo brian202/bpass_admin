64941dfafb8ae262520cb165a82e11c15a8f53db
Jiwon Jung <jiwonish@coinplug.com>
2021-02-16 16:13:19 +0900

    Function: pagenation constructure

--- src/components/Notice/NoticeList.vue
+++ src/components/Notice/NoticeList.vue
@@ -2,8 +2,22 @@
   <div class="NoticeList">
     <h1>공지사항</h1>
     <br />
-    <!-- NoticeList라는 컴포넌트에 pageArray를 넘기기 -->
-    <Pagination :list-array="pageArray" />
+    <table>
+      <tr>
+        <th>NO</th>
+        <th>TEL</th>
+        <th>ADDRESS</th>
+        <th>NAME</th>
+      </tr>
+      <tr v-for="p in pageArray" :key="p.no">
+        <td>{{ p.no }}</td>
+        <td>{{ p.tel }}</td>
+        <td>{{ p.address }}</td>
+        <td>{{ p.name }}</td>
+      </tr>
+    </table>
+
+    <Pagination :counts="pageArray.length" />
   </div>
 </template>
 
@@ -26,9 +40,7 @@ export default class extends Vue {
       .get('http://sample.bmaster.kro.kr/contacts')
       // Axios와 통신 성공 시, 응답 데이터의 contacts를 pageArray에 저장
       .then(response => {
-        console.log(response);
         this.pageArray = response.data.contacts;
-        console.log(this.pageArray);
       })
       .catch(err => {
         console.log(err);
@@ -38,8 +50,35 @@ export default class extends Vue {
 </script>
 
 <style scoped>
-h1 {
-  color: #ffffff;
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+table th {
+  font-size: 1.2rem;
+}
+table tr {
+  height: 2rem;
   text-align: center;
+  border-bottom: 1px solid #505050;
+}
+table tr:first-of-type {
+  border-top: 2px solid #404040;
+}
+table tr td {
+  padding: 1rem 0;
+  font-size: 1.1rem;
+}
+.btn-cover {
+  margin-top: 1.5rem;
+  text-align: center;
+}
+.btn-cover .page-btn {
+  width: 5rem;
+  height: 2rem;
+  letter-spacing: 0.5px;
+}
+.btn-cover .page-count {
+  padding: 0 1rem;
 }
 </style>
--- src/components/Notice/Pagination.vue
+++ src/components/Notice/Pagination.vue
@@ -1,25 +1,10 @@
 <template>
   <div class="pagination">
-    <table>
-      <tr>
-        <th>NO</th>
-        <th>TEL</th>
-        <th>ADDRESS</th>
-        <th>NAME</th>
-      </tr>
-      <tr v-for="p in pageArray" :key="p.no">
-        <td>{{ p.no }}</td>
-        <td>{{ p.tel }}</td>
-        <td>{{ p.address }}</td>
-        <td>{{ p.name }}</td>
-      </tr>
-    </table>
-
     <div class="btn-cover">
       <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
         이전
       </button>
-      <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} 페이지</span>
+      <span class="page-count">{{ pageNum + 1 }} / {{ pageCount() }} 페이지</span>
       <button
         :disabled="pageNum >= pageCount - 1"
         @click="nextPage"
@@ -33,26 +18,13 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import axios from 'axios';
 @Component
 export default class extends Vue {
-  // Data속성
-  pageNum = 0;
-  pageArray = [];
+  @Prop()
+  counts!: number;
 
-  created() {
-    axios
-      .get('http://sample.bmaster.kro.kr/contacts')
-      // Axios와 통신 성공 시, 응답 데이터의 contacts를 pageArray에 저장
-      .then(response => {
-        console.log(response);
-        this.pageArray = response.data.contacts;
-        console.log(this.pageArray);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  // Data속성
+  pageNum = this.counts;
 
   // Methods 속성
   nextPage() {
@@ -62,55 +34,16 @@ export default class extends Vue {
     this.pageNum -= 1;
   }
 
-  @Prop({ required: true }) private listArray!: Array<any>;
-  @Prop({ required: false, default: 10 }) private pageSize!: number;
-
   // Computed 속성
-  pageCount(pageNum: number) {
-    const listLeng = this.listArray.length;
-    const listSize = this.pageSize;
-    const page = Math.floor((listLeng - 1) / listSize) + 1;
-    return page;
+  pageCount() {
+    console.log(this.counts, 'counts');
+    return this.counts;
   }
 
-  paginatedData() {
-    const start = this.pageNum * this.pageSize;
-    const end = start + this.pageSize;
-    return this.listArray.slice(start, end);
-  }
+  // paginatedData() {
+  //   const start = this.pageNum * this.pageSize;
+  //   const end = start + this.pageSize;
+  //   return this.listArray.slice(start, end);
+  // }
 }
 </script>
-
-<style scoped>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-table th {
-  font-size: 1.2rem;
-}
-table tr {
-  height: 2rem;
-  text-align: center;
-  border-bottom: 1px solid #505050;
-}
-table tr:first-of-type {
-  border-top: 2px solid #404040;
-}
-table tr td {
-  padding: 1rem 0;
-  font-size: 1.1rem;
-}
-.btn-cover {
-  margin-top: 1.5rem;
-  text-align: center;
-}
-.btn-cover .page-btn {
-  width: 5rem;
-  height: 2rem;
-  letter-spacing: 0.5px;
-}
-.btn-cover .page-count {
-  padding: 0 1rem;
-}
-</style>
