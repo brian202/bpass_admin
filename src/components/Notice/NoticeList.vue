b2770e9a55c1020e23641a3ec18bc63b75f3a399
brian202 <42294847+brian202@users.noreply.github.com>
2021-02-18 17:00:54 +0900

    페이징 구현 완료

--- src/components/Notice/NoticeList.vue
+++ src/components/Notice/NoticeList.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="app-contatiner">
-    <table>
-      <tr>
-        <td>번호</td>
-        <td>제목</td>
-        <!-- <td>내용</td> -->
-        <td>등록일시</td>
-      </tr>
+  <div class="app-contatiner"> 
 
+
+    <!-- <table>
       <tr v-for="value in pageArray" :key="value.id">
         <th>{{ value.id }}</th>
-        <th>
-          <router-link :to="{ name: 'View' , params: { postId: value.id.toString()}}">
-            {{ value.title }}
-          </router-link>
-        </th>
-        <!-- <th>{{ value.contents }}</th> -->
+        <th>{{ value.title }}</th>
         <th>{{ value.date }}</th>
       </tr>
-    </table>
-    <Pagination :counts="pageArray.length" />
+    </table> -->
+    <Pagination :list-array="pageArray" />
   </div>
 </template>
 
@@ -30,56 +20,23 @@ import Pagination from '@/components/Notice/Pagination.vue';
 // 1. axios 객체 추가
 import api from '@/api';
 
-// 4. store 추가
-import store from 'src/store';
-
-// 5. mapActions 핼퍼 함수 추가
-import { mapActions, mapState } from 'vuex';
-
-
-
-import { State, Action } from 'vuex-class'
-import { Notice } from '@/store/modules/notice'
-
 @Component({
   components: {
-    Pagination,
-  }
-  ,
-  // // 6. mapActions 헬퍼 함수를 컴포넌트에 fetchNoticeList 함수를 매핑
-  computed: {
-    // ...mapActions({
-    //   fetchNoticeList: 'fetchNoticeList'
-    // })
-    // ,
-    // ...mapState({
-    //   pageArray : 'pageArray'
-    // })
+    Pagination
   }
 })
 export default class extends Vue {
 
-  // 2. Data 생성
   pageArray = [];
 
   created() {
-    // 3. created 훅에서 API 호출 및, 컴포넌트 내의 데이터에 해당 결과값 대입
+
     api.get('/t_notice')
       .then(response => {
         this.pageArray = response.data;
 
       })
   }
-
-  // 7. 매핑된 함수를 실행
-  fetchNoticeList!: () => any;
-
-  // @State readonly pageArray!: Notice[]
-  // @Action readonly fetchNoticeList: any
-  // created() {
-  //   this.pageArray = this.fetchNoticeList()
-  // }
-
 
 }
 </script>
--- src/components/Notice/Pagination.vue
+++ src/components/Notice/Pagination.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="pagination">
+    <table>
+      <tr>
+        <td>번호</td>
+        <td>제목</td>
+        <!-- <td>내용</td> -->
+        <td>등록일시</td>
+      </tr>
+      <tr v-for="(val, key) in pagelist()" v-bind:key="key">
+      <!-- {{ pagelist() }} -->
+        <th>{{val.id}}</th>
+        <th>{{val.title}}</th>
+        <th>{{val.date}}</th>
+      </tr>
+    </table>
+
     <div class="btn-cover">
       <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
         이전
       </button>
       <span class="page-count">{{ pageNum + 1 }} / {{ pageCount() }} 페이지</span>
       <button
-        :disabled="pageNum >= pageCount - 1"
-        @click="nextPage"
-        class="page-btn"
-      >
+        :disabled="pageNum >= pageCount() - 1" @click="nextPage" class="page-btn">
         다음
       </button>
     </div>
@@ -18,13 +30,25 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import api from '@/api';
+
 @Component
 export default class extends Vue {
-  @Prop()
-  counts!: number;
+  @Prop({
+      type: Array,
+      required: true
+    }) listArray!: string[];
+  @Prop({
+      type: Number,
+      required: false,
+      default: 10
+    })
 
   // Data속성
-  pageNum = this.counts;
+  pageNum = 0;
+  pageSize = 10;
+  p = this.pagelist();
+
 
   // Methods 속성
   nextPage() {
@@ -36,14 +60,20 @@ export default class extends Vue {
 
   // Computed 속성
   pageCount() {
-    console.log(this.counts, 'counts');
-    return this.counts;
+    const listLeng = this.listArray.length;
+    const listSize = this.pageSize;
+    let page = Math.floor(listLeng / listSize);
+      if (listLeng % listSize > 0) page += 1;
+
+      return page;
   }
 
-  // paginatedData() {
-  //   const start = this.pageNum * this.pageSize;
-  //   const end = start + this.pageSize;
-  //   return this.listArray.slice(start, end);
-  // }
+  pagelist() {
+    const start = this.pageNum * this.pageSize;
+    const end = start + this.pageSize;
+    return this.listArray.slice(start, end);
+  } 
+ 
+
 }
 </script>
